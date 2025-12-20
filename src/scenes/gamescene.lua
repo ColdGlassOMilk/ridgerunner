e512ac92fdc2817c9f7603012ccfe98a7cb34323
Nick Brabant <nick.brabant@newwavena.com>
2025-12-20 13:00:11 -0800

    working game

--- src/scenes/gamescene.lua
+++ src/scenes/gamescene.lua
@@ -1,36 +1,376 @@
-
 gamescene = {}
 
 function gamescene:init()
-  -- input:bind({
-  --   [input.button.x] = function() player_menu:show() end,
-  --   [input.button.o] = function() player_menu:hide() end
-  -- })
+  -- game data
+  self.wave = 1
+  self.battle_timer = 0
+  self.battle_msg = ""
+  self.msg_timer = 0
+  self.gold = 0
 
-  self.fsm = state:new({
-    main = {
-      bindings = {
-        [input.button.x] = function() self.fsm:switch('p_menu') end,
-        [input.button.o] = function() scene:switch('title') end
-      }
+  self.player = {
+    x = 20,
+    y = 80,
+    base_y = 80,
+    hp = 10,
+    max_hp = 10,
+    atk = 3,
+    armor = 0,
+    spr = 16
+  }
+  self.enemy = nil
+
+  -- upgrade costs (increase after each purchase)
+  self.costs = {
+    atk = 10,
+    hp = 8,
+    armor = 15
+  }
+
+  -- submenus for player menu
+  local inv_menu = menu:new({
+    {label = 'uSE iTEM', action = function() return true end},
+    {label = 'dROP iTEM', action = function() return true end},
+    {label = 'eXAMINE', action = function() return true end}
+  })
+
+  local shop_menu = menu:new({
+    {
+      label = function()
+        return 'aTK +1 (' .. self.costs.atk .. 'g)'
+      end,
+      enabled = function()
+        return self.gold >= self.costs.atk
+      end,
+      action = function()
+        self.gold -= self.costs.atk
+        self.player.atk += 1
+        self.costs.atk = flr(self.costs.atk * 1.5)
+        sfx(5, 3)
+      end
     },
-    p_menu = {
-      bindings = {
-        [input.button.o] = function() self.fsm:switch('main') end
-      },
-      init = function() player_menu:show() end,
-      exit = function() player_menu:hide() end
+    {
+      label = function()
+        return 'hP +5 (' .. self.costs.hp .. 'g)'
+      end,
+      enabled = function()
+        return self.gold >= self.costs.hp
+      end,
+      action = function()
+        self.gold -= self.costs.hp
+        self.player.max_hp += 5
+        self.player.hp += 5
+        self.costs.hp = flr(self.costs.hp * 1.5)
+        sfx(5, 3)
+      end
+    },
+    {
+      label = function()
+        return 'aRMOR +1 (' .. self.costs.armor .. 'g)'
+      end,
+      enabled = function()
+        return self.gold >= self.costs.armor
+      end,
+      action = function()
+        self.gold -= self.costs.armor
+        self.player.armor += 1
+        self.costs.armor = flr(self.costs.armor * 1.5)
+        sfx(5, 3)
+      end
     }
-  }, 'main')
+  })
+
+  local save_menu = menu:new({
+    {label = 'sAVE gAME', action = function() return true end},
+    {label = 'lOAD gAME', action = function() return true end},
+    {label = 'qUIT', action = function() scene:switch('title') end}
+  })
+
+  player_menu = menu:new({
+    {spr = 1, sub_menu = inv_menu},
+    {spr = 2, sub_menu = shop_menu},
+    {spr = 3, sub_menu = save_menu},
+  }, nil, 112, {
+    horizontal = true,
+    icon_size = 8,
+    spacing = 4
+  })
+
+  -- state machine
+  self.fsm = state:new({
+    walking = {
+      init = function(s, data)
+        self:spawn_enemy()
+        -- player idle bob
+        tween:cancel_all(self.player)
+        tween:loop(self.player, {y = self.player.base_y - 2}, 20, {ease = tween.ease.in_out_quad})
+      end,
+      update = function(s, data)
+        mountains:update()
+      end
+    },
+
+    battle = {
+      init = function(s, data)
+        self.battle_timer = 30
+        self.player_turn = true
+        self:show_msg("eNEMY aPPROACHED!")
+
+        -- stop enemy horizontal movement, keep bobbing
+        if self.enemy then
+          tween:cancel_all(self.enemy)
+          tween:loop(self.enemy, {y = self.enemy.base_y - 3}, 15, {ease = tween.ease.in_out_quad})
+        end
+      end,
+      update = function(s, data)
+        self.battle_timer -= 1
+
+        if self.battle_timer <= 0 then
+          if self.player_turn then
+            self:player_attack()
+          else
+            self:enemy_attack()
+          end
+          self.battle_timer = 30
+        end
+      end
+    },
+
+    victory = {
+      init = function(s, data)
+        -- gold reward scales with wave
+        local reward = 5 + self.wave * 2
+        self.gold += reward
+        self:show_msg("vICTORY! +" .. reward .. "g")
+        self.victory_timer = 60
+        self:reset_player()
+      end,
+      update = function(s, data)
+        self.victory_timer -= 1
+        if self.victory_timer <= 0 then
+          self.wave += 1
+          self.fsm:switch('walking')
+        end
+      end
+    },
+
+    defeat = {
+      init = function(s, data)
+        self:show_msg("dEFEATED!")
+        self.defeat_timer = 90
+
+        -- player death animation
+        tween:cancel_all(self.player)
+        tween:new(self.player, {y = self.player.base_y + 4}, 10, {ease = tween.ease.out_quad})
+      end,
+      update = function(s, data)
+        self.defeat_timer -= 1
+        if self.defeat_timer <= 0 then
+          self:reset_player()
+          self.fsm:switch('walking')
+        end
+      end
+    }
+  }, 'walking')
+
+  -- scene-level input
+  input:bind({
+    [input.button.x] = function()
+      if not player_menu.active then
+        player_menu:show()
+      end
+    end,
+    [input.button.o] = function()
+      if not player_menu.active then
+        self.gold += 1
+        sfx(2, 3)
+      end
+    end
+  })
+end
+
+function gamescene:spawn_enemy()
+  local base_hp = 5 + self.wave * 2
+  local base_atk = 1 + flr(self.wave / 2)
+  local base_y = 80
+
+  self.enemy = {
+    x = 140,
+    y = base_y,
+    base_y = base_y,
+    hp = base_hp,
+    max_hp = base_hp,
+    atk = base_atk,
+    spr = 32
+  }
+
+  -- calculate tween duration based on distance
+  local target_x = self.player.x + 16
+  local distance = self.enemy.x - target_x
+  local duration = distance * (2 - self.wave * 0.05)  -- faster each wave
+  duration = max(duration, 60)  -- minimum 1 second
+
+  -- smooth approach tween
+  tween:cancel_all(self.enemy)
+  tween:new(self.enemy, {x = target_x}, duration, {
+    ease = tween.ease.out_quad,
+    on_complete = function()
+      self.fsm:switch('battle')
+    end
+  })
+
+  -- bobbing while walking
+  tween:loop(self.enemy, {y = base_y - 4}, 12, {ease = tween.ease.in_out_quad})
+end
+
+function gamescene:reset_player()
+  self.player.hp = self.player.max_hp
+  self.player.y = self.player.base_y
+end
+
+function gamescene:show_msg(msg)
+  self.battle_msg = msg
+  self.msg_timer = 45
+end
+
+function gamescene:player_attack()
+  if not self.enemy then return end
+
+  -- attack lunge animation
+  local orig_x = self.player.x
+  tween:new(self.player, {x = orig_x + 8}, 6, {
+    ease = tween.ease.out_quad,
+    on_complete = function()
+      tween:new(self.player, {x = orig_x}, 6, {ease = tween.ease.in_quad})
+    end
+  })
+
+  local dmg = self.player.atk + flr(rnd(3))
+  self.enemy.hp -= dmg
+  self:show_msg("hIT FOR " .. dmg .. " dMG!")
+  sfx(5, 3)
+
+  -- enemy hit reaction
+  local enemy_orig_x = self.enemy.x
+  tween:new(self.enemy, {x = enemy_orig_x + 4}, 4, {
+    ease = tween.ease.out_quad,
+    on_complete = function()
+      tween:new(self.enemy, {x = enemy_orig_x}, 8, {ease = tween.ease.out_elastic})
+    end
+  })
+
+  if self.enemy.hp <= 0 then
+    -- death animation
+    tween:cancel_all(self.enemy)
+    tween:new(self.enemy, {y = self.enemy.base_y + 20, x = self.enemy.x + 10}, 30, {
+      ease = tween.ease.in_quad,
+      on_complete = function()
+        self.enemy = nil
+        self.fsm:switch('victory')
+      end
+    })
+    return
+  end
+
+  self.player_turn = false
+end
+
+function gamescene:enemy_attack()
+  if not self.enemy then return end
+
+  -- enemy lunge animation
+  local orig_x = self.enemy.x
+  tween:new(self.enemy, {x = orig_x - 8}, 6, {
+    ease = tween.ease.out_quad,
+    on_complete = function()
+      tween:new(self.enemy, {x = orig_x}, 6, {ease = tween.ease.in_quad})
+    end
+  })
+
+  -- apply armor reduction
+  local dmg = max(1, self.enemy.atk + flr(rnd(2)) - self.player.armor)
+  self.player.hp -= dmg
+  self:show_msg("eNEMY hIT FOR " .. dmg .. "!")
+  sfx(6, 3)
+
+  -- player hit reaction
+  local player_orig_x = self.player.x
+  tween:new(self.player, {x = player_orig_x - 4}, 4, {
+    ease = tween.ease.out_quad,
+    on_complete = function()
+      tween:new(self.player, {x = player_orig_x}, 8, {ease = tween.ease.out_elastic})
+    end
+  })
+
+  if self.player.hp <= 0 then
+    self.player.hp = 0
+    self.fsm:switch('defeat')
+    return
+  end
+
+  self.player_turn = true
+end
+
+function gamescene:print_right(txt, y, col)
+  local w = print(txt, 0, -100)
+  print(txt, 126 - w, y, col)
 end
 
 function gamescene:update()
-  mountains:update()
+  self.fsm:update()
+  player_menu:update()
+
+  if self.msg_timer > 0 then
+    self.msg_timer -= 1
+  end
 end
 
 function gamescene:draw()
   cls()
   mountains:draw()
+
+  -- player
+  spr(self.player.spr, self.player.x, self.player.y)
+
+  -- enemy
+  if self.enemy then
+    spr(self.enemy.spr, self.enemy.x, self.enemy.y)
+  end
+
+  -- left side: wave and gold
+  print("wAVE " .. self.wave, 2, 2, 6)
+  print("★" .. self.gold, 2, 10, 10)
+
+  -- right side: player stats
+  self:print_right("aTK " .. self.player.atk, 2, 8)
+  self:print_right("aRM " .. self.player.armor, 10, 12)
+  self:print_right("hP " .. self.player.hp .. "/" .. self.player.max_hp, 18, 11)
+
+  -- player hp bar
+  local bar_w = 30
+  local hp_pct = mid(0, self.player.hp / self.player.max_hp, 1)
+  rectfill(10, 70, 10 + bar_w, 74, 1)
+  rectfill(10, 70, 10 + bar_w * hp_pct, 74, 11)
+  rect(10, 70, 10 + bar_w, 74, 5)
+
+  -- enemy hp bar
+  if self.enemy then
+    local ehp_pct = mid(0, self.enemy.hp / self.enemy.max_hp, 1)
+    rectfill(88, 70, 88 + bar_w, 74, 1)
+    rectfill(88, 70, 88 + bar_w * ehp_pct, 74, 8)
+    rect(88, 70, 88 + bar_w, 74, 5)
+  end
+
+  -- battle message
+  if self.msg_timer > 0 then
+    local w = print(self.battle_msg, 0, -100)
+    print(self.battle_msg, 64 - w / 2, 30, 7)
+  end
+
+  -- menu hint
+  if not player_menu.active then
+    print("❎ mENU  🅾️ +1g", 2, 120, 6)
+  end
 
   player_menu:draw()
 end
--- src/system/menu.lua
+++ src/system/menu.lua
@@ -25,7 +25,10 @@ function menu:new(items, x, y, opts)
     show_bg = opts.show_bg != false,
     show_border = opts.show_border != false,
     show_shadow = opts.show_shadow != false,
-    closeable = opts.closeable != false
+    closeable = opts.closeable != false,
+    horizontal = opts.horizontal or false,
+    icon_size = opts.icon_size or 8,
+    spacing = opts.spacing or 2
   }
   setmetatable(m, self)
   return m
@@ -34,17 +37,21 @@ end
 -- Calculate menu dimensions
 function menu:get_dimensions()
   local padding = 3
-  local spacing = 2
-  local font_h = 6
 
-  local w = 0
-  for item in all(self.items) do
-    w = max(w, print(self:get_label(item), 0, -100))
+  if self.horizontal then
+    local w = #self.items * self.icon_size + (#self.items - 1) * self.spacing + padding * 2
+    local h = self.icon_size + padding * 2
+    return w, h
+  else
+    local font_h = 6
+    local w = 0
+    for item in all(self.items) do
+      w = max(w, print(self:get_label(item), 0, -100))
+    end
+    local menu_w = w + padding * 2
+    local menu_h = padding * 2 + font_h * #self.items + self.spacing * (#self.items - 1)
+    return menu_w, menu_h
   end
-  local menu_w = w + padding * 2
-  local menu_h = padding * 2 + font_h * #self.items + spacing * (#self.items - 1)
-
-  return menu_w, menu_h
 end
 
 -- Show menu
@@ -76,11 +83,17 @@ end
 
 -- internal: bind menu navigation
 function menu:_bind_input()
-  local bindings = {
-    [input.button.up] = function() self:navigate(-1) end,
-    [input.button.down] = function() self:navigate(1) end,
-    [input.button.x] = function() self:select() end
-  }
+  local bindings = {}
+
+  if self.horizontal then
+    bindings[input.button.left] = function() self:navigate(-1) end
+    bindings[input.button.right] = function() self:navigate(1) end
+  else
+    bindings[input.button.up] = function() self:navigate(-1) end
+    bindings[input.button.down] = function() self:navigate(1) end
+  end
+
+  bindings[input.button.x] = function() self:select() end
 
   if self.closeable then
     bindings[input.button.o] = function() self:hide() end
@@ -138,6 +151,14 @@ function menu:select()
 
   -- open sub-menu
   if selected_item.sub_menu then
+    -- position sub-menu before showing
+    if self.horizontal then
+      local sub = selected_item.sub_menu
+      local sub_w, sub_h = sub:get_dimensions()
+      local ix, iy = self:get_icon_pos(self.sel)
+      sub.x = ix + self.icon_size / 2 - sub_w / 2
+      sub.y = iy - sub_h - 4
+    end
     selected_item.sub_menu:show(self)
     return
   end
@@ -172,17 +193,23 @@ function menu:update()
   end
 end
 
+-- Get icon position for horizontal menus
+function menu:get_icon_pos(i)
+  local padding = 3
+  local menu_w, menu_h = self:get_dimensions()
+  local draw_x = mid(0, self.x, 128 - menu_w - 2)
+  local ix = draw_x + padding + (i - 1) * (self.icon_size + self.spacing)
+  local iy = self.draw_y + padding
+  return ix, iy
+end
+
 -- Draw menu
 function menu:draw()
   if not self.visible then return end
 
   local padding = 3
-  local spacing = 2
-  local font_h = 6
-
   local menu_w, menu_h = self:get_dimensions()
 
-  -- use draw_y for animation, x stays fixed
   local draw_x = mid(0, self.x, 128 - menu_w - 2)
   local draw_y = self.draw_y
 
@@ -201,30 +228,65 @@ function menu:draw()
     rect(draw_x, draw_y, draw_x + menu_w, draw_y + menu_h, self.bordcol)
   end
 
-  -- items
-  for i = 1, #self.items do
-    local item_x = draw_x + padding
-    local item_y = draw_y + padding + (font_h + spacing) * (i - 1)
-    local item = self.items[i]
-    local is_selected = i == self.sel
-    local is_disabled = not self:is_enabled(item)
-    local label = self:get_label(item)
+  if self.horizontal then
+    -- horizontal icon layout
+    for i = 1, #self.items do
+      local item = self.items[i]
+      local ix, iy = self:get_icon_pos(i)
+      local is_selected = i == self.sel
+      local is_disabled = not self:is_enabled(item)
 
-    local shadow_col = is_selected and 0 or 1
-    local text_col = is_disabled and 5 or (is_selected and 12 or 6)
+      -- selection highlight
+      if is_selected then
+        rectfill(ix - 1, iy - 1, ix + self.icon_size, iy + self.icon_size, 12)
+      end
 
-    print(label, item_x + 1, item_y + 1, shadow_col)
-    print(label, item_x, item_y, text_col)
-  end
+      -- sprite (use pal for disabled look)
+      if is_disabled then
+        pal(7, 5)
+      end
+      spr(item.spr, ix, iy)
+      pal()
+    end
 
-  -- sub-menu
-  local selected_item = self.items[self.sel]
-  if selected_item.sub_menu and selected_item.sub_menu.active then
-    local sub = selected_item.sub_menu
-    sub.x = draw_x + menu_w + 2
-    sub.y = draw_y + (font_h + spacing) * (self.sel - 1)
-    sub.draw_y = sub.y  -- sync draw_y for sub-menus
-    spr(0, sub.x - 11, sub.y + padding)
-    sub:draw()
+    -- draw sub-menu for horizontal
+    local selected_item = self.items[self.sel]
+    if selected_item.sub_menu and selected_item.sub_menu.active then
+      local sub = selected_item.sub_menu
+      local sub_w, sub_h = sub:get_dimensions()
+      local ix, iy = self:get_icon_pos(self.sel)
+      sub.x = ix + self.icon_size / 2 - sub_w / 2
+      sub.y = iy - sub_h - 4
+      sub:draw()
+    end
+  else
+    -- vertical text layout
+    local font_h = 6
+    for i = 1, #self.items do
+      local item_x = draw_x + padding
+      local item_y = draw_y + padding + (font_h + self.spacing) * (i - 1)
+      local item = self.items[i]
+      local is_selected = i == self.sel
+      local is_disabled = not self:is_enabled(item)
+      local label = self:get_label(item)
+
+      local shadow_col = is_selected and 0 or 1
+      local text_col = is_disabled and 5 or (is_selected and 12 or 6)
+
+      print(label, item_x + 1, item_y + 1, shadow_col)
+      print(label, item_x, item_y, text_col)
+    end
+
+    -- draw sub-menu for vertical
+    local selected_item = self.items[self.sel]
+    if selected_item.sub_menu and selected_item.sub_menu.active then
+      local sub = selected_item.sub_menu
+      local font_h = 6
+      sub.x = draw_x + menu_w + 2
+      sub.y = draw_y + (font_h + self.spacing) * (self.sel - 1)
+      sub.draw_y = sub.y
+      spr(0, sub.x - 11, sub.y + padding)
+      sub:draw()
+    end
   end
 end
